87e665dbba1207308ebc86c0ed892739b55831a6
fecaridade <caridade500@gmail.com>
2019-11-09 23:15:51 -0300

    Biblioteca 2v

---
+++ OO-car.cpp
@@ -0,0 +1,63 @@
+/*
+autor: Felipe Caridade
+desc: Controlar um carro de controle remoto usando OO
+*/
+
+#include "Arduino.h"
+#include "OO-car.h"
+
+
+//Construtor - Metodos
+OOCarMotor::OOCarMotor(int pino_In1, int pino_In2, int pino_pwm){
+    _pino_In1 = pino_In1;
+    _pino_In2 = pino_In2;
+    _pino_pwm = pino_pwm;
+     pinMode(_pino_In1, OUTPUT);
+     pinMode(_pino_In2, OUTPUT);
+     pinMode(_pino_pwm, OUTPUT);
+}
+//Velocidade - Metodos
+OOCarMotor::setSpeed(int velocidade){
+    _velocidade = velocidade;
+}
+
+//Direção - Metodos
+OOCarMotor::run(int direcao){
+    _direcao = direcao
+
+    switch (_direcao) {
+	    case FORWARD:
+	        digitalWrite(_pino_In1, HIGH);
+	        digitalWrite(_pino_In2, LOW);
+	        break;
+	    case BACKWARD:
+	        digitalWrite(_pino_In1, LOW);
+	        digitalWrite(_pino_In2, HIGH);
+	        break;
+	    case BRAKE:
+	        digitalWrite(_pino_In1, HIGH);
+	        digitalWrite(_pino_In2, HIGH);
+	        break;
+	    case RELEASE:
+	        digitalWrite(_pino_In1, LOW);
+	        digitalWrite(_pino_In2, LOW);
+	      break;
+    
+
+	}
+}
+
+OOCarControlLed::OOCarControlLed(int pino_led){
+    _pino_led = pino_led;
+
+    pinMode(_pino_led, OUTPUT);
+}
+
+OOCarControlLed::acender(){
+    pinMode(_pino_led,HIGH);
+}
+
+OOCarControlLed::apagar(){
+    pinMode(_pino_led,LOW);
+}
+
---
+++ OO-car.h
@@ -0,0 +1,45 @@
+/*
+autor: Felipe Caridade 
+desc: Controlar um carro de controle remoto usando OO
+*/
+
+#ifndef Carro_OO_h
+#define Carro_OO_h
+#include "Arduino.h"
+
+
+// Variavies que guardam o valor para onde o carro deverá ir
+#define FORWARD  1
+#define BACKWARD 2
+#define BRAKE    3
+#define RELEASE  4
+
+
+
+//Classe de controle do motor
+class OOCarMotor
+{
+    public:
+        OOCarMotor(int pino_In1, int pino_In2, int pino_pwm);
+	    void setSpeed(int velocidade);
+	    void run(int direcao);
+
+    private:
+        int _pino_In1, _pino_In2, _pino_pwm;
+		int _velocidade;
+		int _direcao;
+};
+// Classe para controle dos Led´s
+class OOCarControlLed
+{
+    public:
+        OOCarControlLed(int pino_led);
+	    void acender();
+        void apagar();
+
+
+    private:
+        int _pino_led
+};
+
+#endif
